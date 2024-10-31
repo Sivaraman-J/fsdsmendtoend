9dc7041368163813589f7941d04859f9af87fda0
Sivaraman <sivaramanperumal@outlook.com>
2024-10-31 16:32:50 +0530

    Data Transformation Code Updated

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -42,6 +42,13 @@ class DataIngestion:
             test_data.to_csv(self.ingestion_config.test_data_path,index=False)
             
             logging.info("Data Ingestion Completed")
+            
+            return (
+                 
+                
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
         
         except Exception as e:
             logging.info("Exception during occured at data ingestion stage")

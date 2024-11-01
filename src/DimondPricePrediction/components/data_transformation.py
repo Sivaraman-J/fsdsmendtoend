6e3bc37bc47d23ec68370ed45fd78121460f3fd7
Sivaraman <sivaramanperumal@outlook.com>
2024-11-01 14:19:58 +0530

    Model training completed

--- src/DimondPricePrediction/components/data_transformation.py
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -94,15 +94,14 @@ class DataTransformation:
             drop_columns = [target_column_name,'id']
             
             input_feature_train_df = train_df.drop(columns=drop_columns,axis=1)
-            target_feature_train_df=train_df[target_column_name]
+            target_feature_train_df = train_df[target_column_name]
             
             
-            input_feature_test_df=test_df.drop(columns=drop_columns,axis=1)
-            target_feature_test_df=test_df[target_column_name]
+            input_feature_test_df = test_df.drop(columns=drop_columns,axis=1)
+            target_feature_test_df = test_df[target_column_name]
             
-            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            
-            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
             
             logging.info("Applying preprocessing object on training and testing datasets.")
             
@@ -110,8 +109,8 @@ class DataTransformation:
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
             save_object(
-                file_path=self.data_transformation_config.preprocessor_obj_file_path,
-                obj=preprocessing_obj
+                file_path = self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessing_obj
             )
             
             logging.info("preprocessing pickle file saved")
